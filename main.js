cf47b154333fffdb77d1b58f37eec4edf2e99e78
Abdullah Adeel <abddev7@gmail.com>
2023-07-02 21:31:39 +0000

    ✨ replace xterm with normal div

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
 const runBtn = document.getElementById("run-btn");
 const clearBtn = document.getElementById("clear-btn");
 const textarea = document.getElementById("python-source");
-// const outputArea = document.getElementById("output-log");
+const outputArea = document.getElementById("output-log");
 
 async function main() {
-  const terminal = new Terminal();
   let pyodide = await loadPyodide({
     indexUrl: "https://cdn.jsdelivr.net/pyodide/v0.23.3/full/",
     stdout: (s) => {
-      terminal.write(`>> ${s}\r\n`);
-      //   const paragraph = document.createElement("p");
-      //   paragraph.innerHTML = `>> ${s}`;
-      //   outputArea.appendChild(paragraph);
+      const paragraph = document.createElement("p");
+      paragraph.innerHTML = `>> ${s}`;
+      outputArea.appendChild(paragraph);
     },
     stderr: (s) => {
-      terminal.write(`>> ${s}\r\n`);
+      const paragraph = document.createElement("p");
+      paragraph.innerHTML = `>> ${s}`;
+      outputArea.appendChild(paragraph);
     },
   });
-  terminal.open(document.getElementById("terminal"));
 
   runBtn?.addEventListener("click", () => {
     console.log("run");
+    outputArea.innerHTML = "";
     const code = textarea.value;
     pyodide.runPython(code);
   });
 
   clearBtn?.addEventListener("click", () => {
-    console.log("clear");
-    terminal.clear();
+    outputArea.innerHTML = "";
   });
 }
 main();
